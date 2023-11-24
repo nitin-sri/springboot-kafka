09b9d0ed26f2292593d1d8c1a5982768ee2e18b6
nitin-srivastav <127774818+nitin-srivastav@users.noreply.github.com>
2023-11-24 09:54:30 +0000

    add springboot kafka project

---
+++ src/main/java/com/example/kafkaexample/config/KafkaTopicConfig.java
@@ -0,0 +1,12 @@
+package com.example.kafkaexample.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    public NewTopic createTopic(){
+        return TopicBuilder.name("myKafkaTopic").build();
+    }
+}
